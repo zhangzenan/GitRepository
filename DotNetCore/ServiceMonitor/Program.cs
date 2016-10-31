59fa4a9fc1e94f09ff357e1bc52eab2166c33040
zhangzenan <zzn1991129@126.com>
2016-10-31 23:26:43 +0800

    控制台程序添加

---
+++ DotNetCore/ServiceMonitor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceMonitor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            Console.WriteLine("This is my first dotnetCore console application!");
+            Console.WriteLine("中文测试");
+            Console.ReadKey();
+        }
+    }
+}
