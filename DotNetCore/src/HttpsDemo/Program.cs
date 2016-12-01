f5a269070dd8e2409fdfde4c128355ad9b770946
zhangzenan <zzn1991129@126.com>
2016-12-01 18:44:38 +0800

    添加httpsDemo

---
+++ DotNetCore/src/HttpsDemo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+
+namespace HttpsDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel(option=> {
+                    option.UseHttps("server.pfx","123.com");
+                })
+                .UseUrls("https://*:443")
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
